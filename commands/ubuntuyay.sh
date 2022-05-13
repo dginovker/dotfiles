88ab0237e9a87f04e3afd37049ed7685a747c8c4
dginovker <danielginovker@gmail.com>
2022-05-13 20:12:57 +0900

    Add autoremove to ubuntu Yay command, replace && with ; so subsequent update commands run if previous one failed

--- commands/ubuntuyay.sh
+++ commands/ubuntuyay.sh
@@ -1,5 +1,5 @@
 # Only run if we're running on Ubuntu
 if lsb_release -a 2> /dev/null | grep -q Ubuntu ; then
-  alias yay="yes Y | sudo apt-get update && yes Y | sudo apt-get upgrade && flatpak update -y"
+  alias yay="yes Y | sudo apt-get update ; yes Y | sudo apt-get upgrade ; yes Y | sudo apt autoremove ; flatpak update -y"
 fi
 
