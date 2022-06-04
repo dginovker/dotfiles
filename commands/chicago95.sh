5ae5433640fc2595c1a84268616c4f5a6094cb42
dginovker <danielginovker@gmail.com>
2022-06-04 13:34:31 +0900

    Add chicago95 source

---
+++ commands/chicago95.sh
@@ -0,0 +1,9 @@
+if grep -q "<property name=\"ThemeName\" type=\"string\" value=\"Chicago95\"/>" ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml 2>/dev/null; then
+  ZSH_THEME="Chicago95"
+
+  echo ""
+  echo ""
+  echo "Microsoft(R) Windows 95"
+  echo "   (C)Copyright Microsoft Corp 1981-1996."
+  echo ""
+fi
