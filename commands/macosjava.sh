5aafc4c8b32cbe855931e5028c0d5c9cc5d5bbbe
dginovker <danielginovker@gmail.com>
2022-07-06 15:43:32 +0900

    Add macOS java switching commands

---
+++ commands/macosjava.sh
@@ -0,0 +1,9 @@
+# Only run if we're running on MacOS
+if uname 2> /dev/null | grep -q Darwin ; then
+  alias j12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
+  alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
+  alias j10="export JAVA_HOME=`/usr/libexec/java_home -v 10`; java -version"
+  alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`; java -version"
+  alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+  alias j7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`; java -version"
+fi
